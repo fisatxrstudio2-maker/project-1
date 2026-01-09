7a7b15fdda8830b9bda0a78f8a08cd71ec397013
fisatxrstudio2-maker <fisatxrstudio2@gmail.com>
2026-01-09 10:59:19 +0530

    Update h1 text with a longer string

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>1234567890</h1>
+      <h1>12345678900000000000000000000000000000</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
